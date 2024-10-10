0e3900ced2ddb0b92fac18987c366bd9304a2623
chairul <chairul.amri@ebconnect.com>
2024-10-10 19:55:43 +0700

    initial type array for array biasa & tuple array

--- bundle/index.js
+++ bundle/index.js
@@ -2,11 +2,18 @@
 let myName = 'Chairul Amri';
 let username = 123;
 let isDead = false;
-// initialization awal kosong nilai
+// // initialization awal kosong nilai
+// let pacarSaya: number | string | boolean;
+// // pacar saya diisi nilainya
+// pacarSaya = '10 ratus bro!';
+// array biasa
 let pacarSaya;
-// pacar saya diisi nilainya
-pacarSaya = '10 ratus bro!';
+pacarSaya = ['Tuti', 'Alawiah'];
+// tuple array
+let saudaraSaya;
+saudaraSaya = ['Nopi', 24, true];
 console.log({ myName });
 console.log({ username });
 console.log({ isDead });
 console.log({ pacarSaya });
+console.log({ saudaraSaya });
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,22 @@ let myName: string = 'Chairul Amri';
 let username: number = 123;
 let isDead: boolean = false;
 
-// initialization awal kosong nilai
-let pacarSaya: number | string | boolean;
+// // initialization awal kosong nilai
+// let pacarSaya: number | string | boolean;
 
-// pacar saya diisi nilainya
-pacarSaya = '10 ratus bro!';
+// // pacar saya diisi nilainya
+// pacarSaya = '10 ratus bro!';
+
+// array biasa
+let pacarSaya: string[];
+pacarSaya = ['Tuti', 'Alawiah'];
+
+// tuple array
+let saudaraSaya: [string, number, boolean];
+saudaraSaya = ['Nopi', 24, true];
 
 console.log({ myName });
 console.log({ username });
 console.log({ isDead });
 console.log({ pacarSaya });
+console.log({ saudaraSaya });
