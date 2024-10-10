75e433ce9ef5e2b3398e6c332c04459c7dadb95e
chairul <chairul.amri@ebconnect.com>
2024-10-10 17:01:17 +0700

    init value default kosong

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ let myName: string = 'Chairul Amri';
 let username: number = 123;
 let isDead: boolean = false;
 
-let pacarSaya;
+// initialization awal kosong nilai
+let pacarSaya: number;
+
+// pacar saya diisi nilainya
+pacarSaya = 5;
 
 console.log({ myName });
 console.log({ username });
