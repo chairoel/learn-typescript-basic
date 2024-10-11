cbbf4e1a95f447a4b52ed61695a361c715dfea1a
chairul <chairul.amri@ebconnect.com>
2024-10-11 14:45:50 +0700

    param on function in TS

--- bundle/fungtion.js
+++ bundle/fungtion.js
@@ -8,3 +8,13 @@ function create() {
 const create2 = () => 'Hello World';
 console.log(create());
 console.log(create2());
+function add(x, y) {
+    const z = x + y;
+    console.log(`hasil adalah: ${z}`);
+}
+function add2(x, y) {
+    return `${x} ditambah ${y} hasilnya: ${x + y}`;
+}
+add(30, 20);
+const result = add2(23, 43);
+console.log(result);
--- src/fungtion.ts
+++ src/fungtion.ts
@@ -10,3 +10,17 @@ const create2 = (): string => 'Hello World';
 
 console.log(create());
 console.log(create2());
+
+function add(x: number, y: number): void {
+  const z: number = x + y;
+  console.log(`hasil adalah: ${z}`);
+}
+
+function add2(x: number, y: number): string {
+  return `${x} ditambah ${y} hasilnya: ${x + y}`;
+}
+
+add(30, 20);
+
+const result = add2(23, 43);
+console.log(result);
